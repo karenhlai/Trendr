d6377922a1286c4abd4ebe5b9dd2374cce0a83c8
Karen <laikarenh@gmail.com>
2019-05-01 16:48:26 -0700

    Remove nextState from session reducer

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -652,7 +652,6 @@ var sessionReducer = function sessionReducer() {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : _nullUser;
   var action = arguments.length > 1 ? arguments[1] : undefined;
   Object.freeze(state);
-  var nextState = lodash_merge__WEBPACK_IMPORTED_MODULE_1___default()({}, state);
 
   switch (action.type) {
     case _actions_session_actions__WEBPACK_IMPORTED_MODULE_0__["RECEIVE_CURRENT_USER"]:
