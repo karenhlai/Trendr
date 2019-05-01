195fa61c4139232888a49ec82f47402e61eced12
Karen <laikarenh@gmail.com>
2019-04-30 17:19:38 -0700

    Complete User and Session Controllers

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  private
+
+	def login!(user)
+		user.reset_session_token!
+		session[:session_token] = user.session_token
+		@current_user = user
+	end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+	def require_logged_in
+    redirect_to new_session_url unless logged_in?
+	end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+	end
+	
+  def require_logged_out
+    redirect_to user_url(current_user) if logged_in?
+  end
+end
