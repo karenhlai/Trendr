16325c4d4bc3b3ea3f246c6e2a44c1fd1e0e1d27
Karen <laikarenh@gmail.com>
2019-05-01 10:21:33 -0700

    Create frontend Session and User

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+
+const _nullUser = Object.freeze({
+    id: null
+});
+
+const sessionReducer = (state = _nullUser, action) => {
+    Object.freeze(state);
+    let nextState = merge({}, state);
+    
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return nextState[action.currentUser.id];
+        case LOGOUT_CURRENT_USER:
+            return _nullUser;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
--- frontend/tumblr_index.jsx
+++ frontend/tumblr_index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // const root = document.getElementById('root');
+    const store = configureStore();
+
+    // TESTING START
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    // TESTING END
+
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to TumblrIndex</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
