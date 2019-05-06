a93362db0ea69530a6c5de585dd75730f0239395
Karen <laikarenh@gmail.com>
2019-05-06 09:12:55 -0700

    Drop type column, add constraints to title and body columns in posts table

--- db/migrate/20190502171059_create_posts.rb
+++ db/migrate/20190502171059_create_posts.rb
@@ -1,9 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string "type", null: false
-      t.string "title"
-      t.text "body" 
+      t.string "title", null: false
+      t.text "body", null: false
       t.integer "author_id", null: false
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
